f362e9a6ca6262e129d013a9c37dfbabd7999569
cheong38 <cheong0308@gmail.com>
2014-04-26 02:17:27 +0900

    훈련 페이지 구현 및 전체 리팩토링 중 서버의 소스를 받기 위한 커밋

---
+++ js/common.js
@@ -0,0 +1,160 @@
+/**
+ * Created by woo-jin on 2014-04-26.
+ */
+// constans
+var BASE_URL = 'http://ugenieus.cafe24.com:5555/nrApi/number';
+var CANVAS_SIZE = 450;
+var DATA_SIZE = 90;
+var DRAW_THICKNESS = 4;
+var DRAW_COLOR = '#2fc1d8';
+var DRAW_AFTER_R = '240';
+var DRAW_AFTER_G = '232';
+var DRAW_AFTER_B = '140';
+
+// canvas variables
+var canvas;
+var context;
+var isDrawing;
+var x, y;
+
+function requestAPI(params) {
+    var url = BASE_URL + '/' + params.method;
+    $.post(url, params.parameter, params.success);
+}
+
+function reduceCanvas() {
+    var canvasWidth, canvasHeight;
+    var blockSize;
+    var stringifyData;
+
+    var imageData;
+    var i, j, k, l;
+    var offset, index;
+    var count;
+    var color;
+
+    // initialize
+    canvasWidth = canvas.width();
+    canvasHeight = canvas.height();
+    blockSize = canvas.width() / DATA_SIZE;
+    stringifyData = "";
+
+    // get canvas data
+    imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+
+    for (i = 0; i < DATA_SIZE; i++) {
+        for (j = 0; j < DATA_SIZE; j++) {
+            offset = (i * canvasWidth + j) * blockSize;
+
+            count = 0;
+            for (k = 0; k < blockSize; k++) {
+                for (l = 0; l < blockSize; l++) {
+                    index = offset + (k * canvasWidth + l);
+                    if (imageData.data[index*4+3] > 128) { // get alpha value
+                        count++;
+                    }
+                }
+            }
+
+            if (count >= blockSize/2) {
+                stringifyData = stringifyData + '1';
+            } else {
+                stringifyData = stringifyData + '0';
+            }
+
+            // for test
+            color = (count >= blockSize/2) * 255;
+            for (k = 0; k < blockSize; k++) {
+                for (l = 0; l < blockSize; l++) {
+                    index = (offset + (k * canvasWidth + l));
+                    // RGBA
+                    imageData.data[index*4+0] = DRAW_AFTER_R;
+                    imageData.data[index*4+1] = DRAW_AFTER_G;
+                    imageData.data[index*4+2] = DRAW_AFTER_B;
+                    imageData.data[index*4+3] = color;
+                }
+            }
+        }
+    }
+
+    context.putImageData(imageData, 0, 0);
+    return stringifyData;
+}
+
+function drawStart(e) {
+    isDrawing = true;
+    x = e.pageX - canvas.offset().left;
+    y = e.pageY - canvas.offset().top;
+
+    // draw circle
+    context.beginPath();
+    context.arc(x, y, DRAW_THICKNESS, 0, 2 * Math.PI, false);
+    context.fill();
+
+    // move point for next line
+    context.beginPath();
+    context.moveTo(x, y);
+}
+
+function drawMove(e) {
+    if (!isDrawing) return;
+
+    x = e.pageX - canvas.offset().left;
+    y = e.pageY - canvas.offset().top;
+
+    // draw line
+    context.lineTo(x, y);
+    context.stroke();
+
+    // draw circle
+    context.beginPath();
+    context.arc(x, y, DRAW_THICKNESS, 0, 2 * Math.PI, false);
+    context.fill();
+
+    // move point for next line
+    context.beginPath();
+    context.moveTo(x, y);
+}
+
+function drawEnd(e) {
+    var stringifyData;
+    if (!isDrawing) return;
+    isDrawing = false;
+    stringifyData = reduceCanvas();
+    return stringifyData;
+}
+
+function initCanvas() {
+    // initialize
+    canvas = $('#recognition_canvas');
+    canvas[0].width = CANVAS_SIZE;
+    canvas[0].height = CANVAS_SIZE;
+    context = canvas[0].getContext('2d');
+    isDrawing = false;
+
+    // setting draw style
+    context.fillStyle = DRAW_COLOR;
+    context.strokeStyle = DRAW_COLOR;
+    context.lineWidth = DRAW_THICKNESS * 2;
+
+    // add event listener
+    canvas.mousedown(drawStart);
+    canvas.mousemove(drawMove);
+    canvas.mouseup(drawEnd);
+    canvas.mouseleave(drawEnd);
+}
+
+function clearCanvas(e) {
+    context.clearRect(0, 0, canvas.width(), canvas.height());
+    isDrawing = false;
+}
+
+function keyDownHandler (e) {
+    switch (e.keyCode) {
+        case 82:
+            clearCanvas();
+            break;
+        default:
+            break;
+    }
+}
---
+++ js/train.js
@@ -0,0 +1,46 @@
+/**
+ * Created by woo-jin on 2014-04-26.
+ */
+function initialize(jQuery) {
+    // add event listener
+    $('#reset_button').click(clearCanvas);
+    $('#send_button').click(clickSendButtonHandler);
+    $('body').keydown(keyDownHandler);
+
+    // initialize canvas
+    initCanvas();
+}
+
+function getInputNumber() {
+    return $('#training-number').val();
+}
+
+function clickSendButtonHandler(e) {
+    var inputNumber = getInputNumber();
+
+    if (inputNumber == '') {
+        alert('숫자를 입력해주세요.')
+    }
+
+    if (inputNumber < 0 || inputNumber > 9) {
+        alert('0 ~ 9 사이의 숫자를 입력해주세요.');
+    }
+    var sendData = reduceCanvas();
+    requestAPI({
+        method: 'save',
+        parameter: {
+            number: inputNumber,
+            result: sendData
+        },
+        success: function(result) {
+            if (result.result === true) {
+                alert('성공적으로 훈련되었습니다.');
+                clearCanvas();
+            } else {
+                alert('훈련에 실패하였습니다.');
+            }
+        }
+    });
+}
+
+$(document).ready(initialize);
