27c786fe8aad033df62449ae1a6c58e2d1a18265
ByungCheol Lee <lmyl0910@gmail.com>
2014-04-25 22:34:03 +0900

    implement reduction canvas

--- js/function.js
+++ js/function.js
@@ -1,11 +1,72 @@
 // constans
 var DRAW_THICKNESS = 4;
+var DATA_SIZE = 50;
 
 // canvas variables
 var canvas;
 var context;
 var isDrawing;
 var x, y;
+
+function reduceCanvas() {
+	var canvasWidth, canvasHeight;
+	var blockSize;
+	var result;
+
+	var imageData;
+	var i, j, k, l;
+	var offset, index;
+	var count;
+	var color;
+
+	// initialize
+	canvasWidth = canvas.width();
+	canvasHeight = canvas.height();
+	blockSize = canvas.width() / DATA_SIZE;
+	result = "";
+
+	// get canvas data
+	imageData = context.getImageData(0, 0, canvasWidth, canvasHeight);
+
+	for (i = 0; i < DATA_SIZE; i++) {
+		for (j = 0; j < DATA_SIZE; j++) {
+			offset = (i * canvasWidth + j) * blockSize;
+
+			count = 0;
+			for (k = 0; k < blockSize; k++) {
+				for (l = 0; l < blockSize; l++) {
+					index = (offset + (k * canvasWidth + l)) * 4;
+					if (imageData.data[index+3] > 128) {
+						count++;
+					}
+				}
+			}
+
+			if (count >= blockSize/2) {
+				result = result + '1';
+			} else {
+				result = result + '0';
+			}
+
+			// for test
+			color = (count >= blockSize/2) * 255;
+			for (k = 0; k < blockSize; k++) {
+				for (l = 0; l < blockSize; l++) {
+					index = (offset + (k * canvasWidth + l));
+					// RGBA
+					imageData.data[index*4+0] = color;
+					imageData.data[index*4+1] = color;
+					imageData.data[index*4+2] = color;
+					imageData.data[index*4+3] = color;
+				}
+			}
+		}
+	}
+
+	context.putImageData(imageData, 0, 0);
+
+	return result;
+}
 
 function drawStart(x, y) {
 	// draw circle
@@ -43,6 +104,7 @@ function mousemoveCanvasHandler(e) {
 
 function mouseupCanvasHandler(e) {
 	isDrawing = false;
+	reduceCanvas();
 }
 
 function initCanvas() {
@@ -54,6 +116,8 @@ function initCanvas() {
 	y = 0;
 
 	// setting draw style
+	context.fillStyle = "#000000";
+	context.strokeStyle = "#000000";
 	context.lineWidth = DRAW_THICKNESS * 2;
 
 	// add event listener
@@ -62,12 +126,9 @@ function initCanvas() {
 	canvas.mouseup(mouseupCanvasHandler);
 }
 
-
 function clickConfirmButtonHandler(e) {
-	var imageData;
-	imageData = context.createImageData(canvas.width(), canvas.height());
-
-
+	var result = reduceCanvas();
+	// api call
 }
 
 function clickResetButtonHandler(e) {
@@ -75,7 +136,8 @@ function clickResetButtonHandler(e) {
 }
 
 function clickSendButtonHandler(e) {
-	alert('click send.');
+	var result = reduceCanvas();
+	// api call
 }
 
 function initialize(jQuery) {
